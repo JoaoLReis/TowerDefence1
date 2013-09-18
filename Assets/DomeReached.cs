f9e99441bb6f97a3e910245c924b6efe04a530a9
JoaoLReis <jype_13@hotmail.com>
2013-09-18 22:59:36 +0100

    Circle of spheres

---
+++ Assets/DomeReached.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DomeReached : MonoBehaviour {
+	
+	private string _liferock = "Rock";
+	private int _numliveslost = 1;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnCollisionEnter(Collision other){
+		
+		Destroy(other.gameObject);
+		GameObject child = GameObject.Find("Rock"+_numliveslost);
+		Destroy(child);
+		_numliveslost++;
+		
+		
+	}
+}
---
+++ Assets/SpinInCircle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinInCircle : MonoBehaviour {
+	
+	
+	private float movement = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.LookAt(GameObject.Find("CenterOfCircleMarker").GetComponent<Transform>());
+		transform.Translate(Time.deltaTime, 0,0);
+		
+		
+	}
+}
