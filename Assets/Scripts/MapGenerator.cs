63e5f8e62125844e38190b1b3f6ac31902b765cf
JoaoLReis <jype_13@hotmail.com>
2013-09-22 18:49:18 +0100

    Changed Map generator code

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -21,7 +21,7 @@ public class MapGenerator : MonoBehaviour {
 	void Start () {
 		Vector3 pos = transform.position;
 		
-		GameObject node;
+		GameObject node = null;
 		
 		// 0-> four 5x size cubes 
 		// 1-> one 10x size tile
@@ -41,44 +41,87 @@ public class MapGenerator : MonoBehaviour {
 									  { 0, 0, 0, 0, 0,  0, 0,  0, 0, 0,  0, 0 }, 
 									  { 0, 0, 0, 0, 0,  0, 0,  0, 0, 0,  0, 0 }, };
 		
+		
 		for (int i = 0; i <= 12; i++) {
 			for (int z = 0; z <= 11; z++) {
-				if (array2D[i,z] == 0) {
-					Instantiate (TowerTile, new Vector3(pos.x - 5, pos.y, pos.z - 5), Quaternion.identity);
-					Instantiate (TowerTile, new Vector3(pos.x + 5, pos.y, pos.z + 5), Quaternion.identity);
-					Instantiate (TowerTile, new Vector3(pos.x + 5, pos.y, pos.z - 5), Quaternion.identity);
-					Instantiate (TowerTile, new Vector3(pos.x - 5, pos.y, pos.z + 5), Quaternion.identity);
-				} else {
-					if(array2D[i,z] == 1) {
-						Instantiate (PathTile, pos, Quaternion.identity);
-					} else {
-						if(array2D[i,z] == 2) {
-							Instantiate (PathTile, pos, Quaternion.identity);
-							node = Instantiate (Node, new Vector3(pos.x, pos.y + 3, pos.z), Quaternion.identity) as GameObject;
-							node.name = ""+(array2D[i,z] - 1);
-							// note: map is still being generated, so only the start position is recorded for later use
-							StartPos = pos;
-						} else {
-							Instantiate (PathTile, pos, Quaternion.identity);
-							node = Instantiate (Node, new Vector3(pos.x, pos.y + 3, pos.z), Quaternion.identity) as GameObject;
-							node.name = ""+(array2D[i,z] - 1);
-							//needs to be a constant or find a better way to define the end position
-							if(array2D[i,z] == 15) {
-								EndPos = pos;
-							}
-						}
-					}
+				int arrayslot = array2D[i,z];
+				Debug.Log ("ArraySlot: " + arrayslot);
+				
+				switch (arrayslot){
+				case 0:
+					createTowerTile(pos);
+					Debug.Log ("TowerTile");
+					break;
+				
+				 
+				case 1:
+					createPathTile(pos);
+					Debug.Log ("PathTile");
+					break;
+				
+					
+				case 2:
+					createStartTile(pos, arrayslot, node);
+					Debug.Log ("StartTile");
+					break;
+				
+				default:
+					createNodePathTile(pos, arrayslot, node);
+					Debug.Log ("NodePathTile");
+					break;
 				}
+				
 				pos = new Vector3(pos.x, pos.y, pos.z - 20);
 			}
 			pos = new Vector3(pos.x - 20, pos.y, transform.position.z);
 		}
 		//2Dimentional array limits need to be constants instead of 12/11
 		Instantiate(Base, new Vector3(EndPos.x, EndPos.y + 3, EndPos.z), Quaternion.identity);
+		for(int i = 1; i<186; i+=3){
+			Invoke("spawnEnemy", i);			
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Instantiate (Enemy, new Vector3(StartPos.x, StartPos.y + 3, StartPos.z), Quaternion.identity);
+		
 	}
+	
+	private void createTowerTile(Vector3 pos){
+		Instantiate (TowerTile, new Vector3(pos.x - 5, pos.y, pos.z - 5), Quaternion.identity);
+		Instantiate (TowerTile, new Vector3(pos.x + 5, pos.y, pos.z + 5), Quaternion.identity);
+		Instantiate (TowerTile, new Vector3(pos.x + 5, pos.y, pos.z - 5), Quaternion.identity);
+		Instantiate (TowerTile, new Vector3(pos.x - 5, pos.y, pos.z + 5), Quaternion.identity);
+		Debug.Log("TowerTilefunction");
+	}
+	
+	private void createPathTile(Vector3 pos){
+		Instantiate (PathTile, pos, Quaternion.identity);
+		Debug.Log("PathTilefunction");
+	}
+	
+	private void createStartTile(Vector3 pos, int arrayslot, GameObject node){
+		Instantiate (PathTile, pos, Quaternion.identity);
+		node = Instantiate (Node, new Vector3(pos.x, pos.y + 3, pos.z), Quaternion.identity) as GameObject;
+		node.name = ""+(arrayslot - 1);
+		// note: map is still being generated, so only the start position is recorded for later use
+		StartPos = pos;
+		Debug.Log("StartTilefunction");
+	}
+	
+	private void createNodePathTile(Vector3 pos, int arrayslot, GameObject node){
+		Instantiate (PathTile, pos, Quaternion.identity);
+		node = Instantiate (Node, new Vector3(pos.x, pos.y + 3, pos.z), Quaternion.identity) as GameObject;
+		node.name = ""+(arrayslot - 1);
+		//needs to be a constant or find a better way to define the end position
+		if(arrayslot == 15) {
+			EndPos = pos;
+		}
+		Debug.Log("NodePathTilefunction");
+	}
+	
+	void spawnEnemy(){
+		Instantiate (Enemy, new Vector3(StartPos.x, StartPos.y + 3, StartPos.z), Quaternion.identity);		
+	}
+	
 }
